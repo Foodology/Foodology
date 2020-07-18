df8e3ca6ff119b79497a2908f62ff9566e85ae98
Nibraas Khan <nnibraas@gmail.com>
2020-07-17 20:23:35 -0400

    Added list to recipes

--- lib/createHomeCards.dart
+++ lib/createHomeCards.dart
@@ -14,7 +14,8 @@ class CreateHomeCards extends StatefulWidget {
 class _CreateHomeCardsState extends State<CreateHomeCards> {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SingleChildScrollView(
+        physics: ScrollPhysics(),
         child: StreamBuilder<UserData>(
           stream: DatabaseService().userData(),
           builder: (context, snap) {
@@ -22,34 +23,43 @@ class _CreateHomeCardsState extends State<CreateHomeCards> {
             var count = snap.data.private.recommendedRecipes.length;
             Map<String, dynamic> map = snap.data.private.recommendedRecipes;
             return ListView.builder(
+              shrinkWrap: true,
               itemCount: count,
+              physics: NeverScrollableScrollPhysics(),
               itemBuilder: (BuildContext context, int index) {
                 String key = map.keys.elementAt(index);
-                return StreamBuilder(
-                  stream: DatabaseService().recipe(map[key]),
-                  builder: (context, AsyncSnapshot<Recipe> snap2) {
-                    if(snap2.data == null) return CircularProgressIndicator();
-                    return StreamBuilder(
-                      stream: DatabaseService().publicUserInfo(key),
-                      builder: (context, AsyncSnapshot<PublicInfo> snap3) {
-                        if(snap3.data == null) return CircularProgressIndicator();
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                          child: (
-                              HomeCard(
-                                title: snap2.data.title,
-                                author: snap3.data.name,
-                                authorRating: snap3.data.authorRating.toString() + " / 10.0",
-                                recipeRating: snap2.data.recipeRating.toString() + " / 10.0",
-                                authorImage: snap3.data.profilePicture,
-                                links: snap2.data.images,
-                                summary: snap2.data.summary,
-                                detailedSummary: snap2.data.detailedSummary,
-                              )
-                          ),);
-                      },
-                    );
-                  },
+                var countList = List.from(map[key]).length;
+                return ListView.builder(
+                    itemCount: countList,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (BuildContext context, int i) {
+                      return StreamBuilder(
+                        stream: DatabaseService().recipe(map[key][i]),
+                        builder: (context, AsyncSnapshot<Recipe> snap2) {
+                          if(snap2.data == null) return CircularProgressIndicator();
+                          return StreamBuilder(
+                            stream: DatabaseService().publicUserInfo(key),
+                            builder: (context, AsyncSnapshot<PublicInfo> snap3) {
+                              if(snap3.data == null) return CircularProgressIndicator();
+                              return Padding(
+                                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+                                child: (
+                                    HomeCard(
+                                      title: snap2.data.title,
+                                      author: snap3.data.name,
+                                      authorRating: snap3.data.authorRating.toString() + " / 10.0",
+                                      recipeRating: snap2.data.recipeRating.toString() + " / 10.0",
+                                      authorImage: snap3.data.profilePicture,
+                                      links: snap2.data.images,
+                                      summary: snap2.data.summary,
+                                      detailedSummary: snap2.data.detailedSummary,
+                                    )
+                                ),);
+                            },
+                          );
+                        },
+                      );}
                 );
               },
             );
