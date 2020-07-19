a6c56d50a8b1d71b5b03049dcc2f673be6d8f5c8
zaeemakhan <zaeemahadkhan@gmail.com>
2020-07-18 21:09:05 -0400

    Basic bottom navigation bar is done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:foodology/screens/AccountPage.dart';
+import 'package:foodology/screens/AddRecipesPage.dart';
+import 'package:foodology/screens/Home.dart';
+import 'package:foodology/screens/MessagesPage.dart';
+import 'package:foodology/screens/SearchPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(HomePage());
 }
 
- 
-//import 'package:flutter/material.dart';
-//
-//class HomePage extends StatefulWidget {
-//  @override
-//  _HomePageState createState() => _HomePageState();
-//}
-//
-//class _HomePageState extends State<HomePage> {
-//
-//  int _selectedPage = 0;
-//  final pageOptions = [
-//    Text("Page1"),
-//    Text("Page2"),
-//    Text("Page3"),
-//    Text("Page4"),
-//    Text("Page5")
-//  ];
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("HomePage"),
-//      ),
-//      body: pageOptions[_selectedPage],
-//      bottomNavigationBar: BottomNavigationBar(
-//        currentIndex: _selectedPage,
-//        onTap: (int index) {
-//          setState(() {
-//            _selectedPage = index;
-//          });
-//        },
-//        items: [
-//          BottomNavigationBarItem(
-//              icon: Icon(Icons.home),
-//              title: Text("Home")
-//          ),
-//          BottomNavigationBarItem(
-//              icon: Icon(Icons.search),
-//              title: Text("Explore")
-//          ),
-//          BottomNavigationBarItem(
-//              icon: Icon(Icons.add),
-//              title: Text("Add")
-//          ),
-//          BottomNavigationBarItem(
-//              icon: Icon(Icons.message),
-//              title: Text("Message")
-//          ),
-//          BottomNavigationBarItem(
-//              icon: Icon(Icons.account_circle),
-//              title: Text("Profile")
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedPage = 0;
+  final pageOptions = [
+    Home(),
+    SearchPage(),
+    AddRecipesPage(),
+    MessagesPage(),
+    AccountPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BottomNav",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(title: Text("Bottom Nav")),
+          body: pageOptions[_selectedPage],
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selectedPage,
+            onTap: (int index) {
+              setState(() {
+                _selectedPage = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("Home")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  title: Text("Search")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.add),
+                  title: Text("Add Recipes")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.message),
+                  title: Text("Messages")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  title: Text("Account")
+              )
+            ],
+          ),
+      ),
+    );
+  }
+}
