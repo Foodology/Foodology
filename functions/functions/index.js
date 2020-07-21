070e772a07a96dd1859b4bc663e1de7243b6f675
Nibraas Khan <nnibraas@gmail.com>
2020-07-21 18:59:21 -0400

    Added code for like and dislike

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,9 +1,38 @@
-const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Init functions and database
+const functions = require('firebase-functions');
+const { firestore } = require('firebase-admin');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+const db = admin.firestore();
+
+exports.incLikes = functions.https.onRequest((request, response) => {
+  db.collection('RecipesByUser').doc('1weHFMrmWtV2UtFY7za11ODbyb13').collection('Recipes').doc('aHTOzNvYPiApxEP27uZF').update({
+    likes: FieldValue.increment(1)
+  })
+});
+
+exports.decLikes = functions.https.onRequest((request, response) => {
+  db.collection('RecipesByUser').doc('1weHFMrmWtV2UtFY7za11ODbyb13').collection('Recipes').doc('aHTOzNvYPiApxEP27uZF').update({
+    likes: FieldValue.increment(-1)
+  })
+});
+
+// exports.updateLikes = functions.firestore
+//     .document('RecipesByUser/{userId}/Recipes/{recipeID}')
+//     .onUpdate((change, context) => {
+//       // Get an object representing the document
+//       // e.g. {'name': 'Marie', 'age': 66}
+//       const newValue = change.after.data();
+
+//       // ...or the previous value before this update
+//       const previousValue = change.before.data();
+
+//       // access a particular field as you would any JS property
+//       const name = newValue.name;
+
+//       db.collection('RecipesByUser').doc('1weHFMrmWtV2UtFY7za11ODbyb13').collection('Recipes').doc('aHTOzNvYPiApxEP27uZF').update({ likes: FieldValue.increment(50) })
+
+//       functions.logger.info("Hello logs!");
+//     });
